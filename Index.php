b120c25274241e48a481966251172a46a910e4f9
John <howlanjo@mail.gvsu.edu>
2016-12-30 18:33:24 -0500

    e

--- Index.php
+++ Index.php
@@ -1,8 +1,7 @@
 <?php
 	$the_title = 'Home';
-	$the_content = '<p>This is the home page. This
-	is where all the overview information will be displayed.</p>';
-	$ImageLocation = "var/www/html/Raspberry-HTML/Photos/HomePhoto.jpg";
+	$the_content = '<p>This is the home page. This is where all the overview information will be displayed.</p>';
+	$ImageLocation = "Photos/HomePhoto.jpg";
 ?>
 
 <?php include('single.php'); ?>
