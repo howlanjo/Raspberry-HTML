c061ab4400cbdf260448660528611804153144a6
John <howlanjo@mail.gvsu.edu>
2016-12-30 13:25:26 -0500

    test
    testing out echo command

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>HowlandBot</title>
-</head>
-<body>
+	<head>
+		<title>HowlandBot</title>
+	</head>
+	<body>
+		<?php
+			echo "<p> Hi, I am a PHP script!<p>";
+		?>
+
 
 <h1>Page Under Construction</h1>
 <p>Thank you for visiting this page. It is currently under construction right now. Please visit at a later time, for its full potential has not yet been un-veiled.</p>
