6f98c15465456b25d4f0953bf9d16e9382126028
John <howlanjo@mail.gvsu.edu>
2016-12-30 18:32:20 -0500

    d

--- Index.php
+++ Index.php
@@ -2,7 +2,7 @@
 	$the_title = 'Home';
 	$the_content = '<p>This is the home page. This
 	is where all the overview information will be displayed.</p>';
-	$ImageLocation = "/var/www/html/Raspberry-HTML/Photos/HomePhoto.jpg";
+	$ImageLocation = "var/www/html/Raspberry-HTML/Photos/HomePhoto.jpg";
 ?>
 
 <?php include('single.php'); ?>
