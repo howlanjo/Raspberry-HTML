c57da4e94bd368b85d3fd1f6eea364bd19d35e4b
John <howlanjo@mail.gvsu.edu>
2016-12-30 13:55:10 -0500

    Added all nessecary files

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>HowlandBot</title>
-	</head>
-	<body>
-		<?php
-			echo "<p> Hi, I am a PHP script!<p>";
-		?>
+<?php
+	$the_title = 'Home';
+	$the_content = '<p>This is the home page.</p>';
+?>
 
-
-<h1>Page Under Construction</h1>
-<p>Thank you for visiting this page. It is currently under construction right now. Please visit at a later time, for its full potential has not yet been un-veiled.</p>
-
-</body>
-</html>
+<?php include('single.php'); ?>
