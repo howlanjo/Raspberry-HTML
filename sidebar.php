1cc4121cf0ab8a3a078170b676f0d78f395b31ff
John <howlanjo@mail.gvsu.edu>
2016-12-30 18:57:19 -0500

    h

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,5 @@
 			<div id="sidebar">
 				<h3>Sidebar Header</h3>
-				<p>This is your sidebar area</p>
-				<p><?php echo "$the_content . This is after..."; ?></p>
+				//<p>This is your sidebar area</p>
+				<p><?php echo $the_content; ?></p>
 			</div>
