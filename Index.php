a3b304d6297af76d76263717d39426a15a7aa7b8
John <howlanjo@mail.gvsu.edu>
2016-12-30 18:22:40 -0500

    Added home photo

--- Index.php
+++ Index.php
@@ -2,6 +2,7 @@
 	$the_title = 'Home';
 	$the_content = '<p>This is the home page. This
 	is where all the overview information will be displayed.</p>';
+	$ImageLocation = "Photos/HomePhoto.jpg";
 ?>
 
 <?php include('single.php'); ?>
