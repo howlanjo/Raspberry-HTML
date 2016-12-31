f7dcd09668d0b268d9e67f52e4840151ec410a6b
John <howlanjo@mail.gvsu.edu>
2016-12-30 20:17:36 -0500

    dcv

--- Index.php
+++ Index.php
@@ -1,16 +1,6 @@
 <?php
 	$the_title = 'Home';
-	$the_content = "This is the home page. This is where all the overview information will be displayed.";
-	
-	if (file_exists($ImageLocation)) 
-	{
-		echo "Yes, the file was found.";
-	}
-	else
-	{
-		echo "No, the files was NOT found.";
-	}
-	
+	$the_content = '<p>This is the home page. This is where all the overview information will be displayed.</p>';
 	$ImageLocation = "Photos/HomePhoto.jpg";
 ?>
 
