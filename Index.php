a63654befd5689bc4d2ce0bb0e9d067bfe1899df
John <howlanjo@mail.gvsu.edu>
2016-12-30 19:54:28 -0500

    o

--- Index.php
+++ Index.php
@@ -1,6 +1,16 @@
 <?php
 	$the_title = 'Home';
 	$the_content = "This is the home page. This is where all the overview information will be displayed.";
+	
+	if (file_exists($ImageLocation)) 
+	{
+		echo "Yes, the file was found.";
+	}
+	else
+	{
+		echo "No, the files was NOT found.";
+	}
+	
 	$ImageLocation = "Photos/HomePhoto.jpg";
 ?>
 
--- single.php
+++ single.php
@@ -2,7 +2,18 @@
 			<div id="content">
 				<div id="main">
 					<h2><?php echo $the_title; ?></h2>
-					<p><?php echo $ImageLocation; ?></p>
+					<!--<p><?php echo $ImageLocation; ?></p>-->
+					<p>
+					<?php 
+					if (file_exists($ImageLocation)) 
+					{
+						echo "Yes, the file was found.";
+					}
+					else
+					{
+						echo "No, the files was NOT found.";
+					}
+					?></p>
 					<center><img src=$ImageLocation alt="Invalid Photo" ></center>
 				</div>
 				<?php
