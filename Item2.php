5d8406f915bff12a25dd60e1b194c41186697101
John <howlanjo@mail.gvsu.edu>
2016-12-30 20:00:33 -0500

    help2

--- Item2.php
+++ Item2.php
@@ -1,7 +1,7 @@
 <?php
 	$the_title = 'Light Bulbs';
 	$the_content = '<p>This is where the information on the second item will be displayed.</p>';
-	$ImageLocation = "/Photos/LightBulbs.jpg";
+	$ImageLocation = "Photos/LightBulbs.jpg";
 ?>
 
 <?php include('single.php'); ?>
--- Item4.php
+++ Item4.php
@@ -1,7 +1,7 @@
 <?php
 	$the_title = 'Stove';
 	$the_content = '<p>This is where the information on the fourth item will be displayed.</p>';
-	$ImageLocation = "\Photos\Stove.jpg";
+	$ImageLocation = "Photos/Stove.jpg";
 ?>
 
 <?php include('single.php'); ?>
