fa0a473ea5c7277f94dfa57c0ed2cd1276fad92b
John <howlanjo@mail.gvsu.edu>
2017-01-10 19:10:31 -0500

    3c

--- Item3.php
+++ Item3.php
@@ -1,7 +1,7 @@
 <?php
 	$the_title = 'Thermostat';
 	$the_content = '<p>This is where the information on the third item will be displayed.</p>';
-	$ImageLocation = "Photos/Thermostat.jpg";
+	$ImageLocation = "/Photos/Thermostat.jpg";
 ?>
 
 <?php include('single.php'); ?>
