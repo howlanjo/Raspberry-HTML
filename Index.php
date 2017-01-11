328f3b9e3d9f4137f8e515e9a50da22b784426e0
John <howlanjo@mail.gvsu.edu>
2017-01-10 20:37:58 -0500

    sryly

--- Index.php
+++ Index.php
@@ -1,7 +1,6 @@
 <?php
 	$the_title = 'Home';
 	$the_content = '<p>This is the home page. This is where all the overview information will be displayed.</p>';
-	$ImageLocation = "/Photos/HomePhoto.jpg";
 	$ImageLocation = "/Photos/CoffeePhoto.jpg";
 ?>
 
--- Item1.php
+++ Item1.php
@@ -1,7 +1,7 @@
 <?php
 	$the_title = 'Coffee Maker';
 	$the_content = '<p>This is where the information on the first item will be displayed.</p>';
-	$ImageLocation = "/Photos/CoffeePhoto.jpg";
+	$ImageLocation = "/Photos/HomePhoto.jpg";
 ?>
 
 <?php include('single.php'); ?>
