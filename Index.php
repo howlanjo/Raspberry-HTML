e256e26481780891d4af24adc988dcfaab041553
John <howlanjo@mail.gvsu.edu>
2017-01-10 20:37:06 -0500

    ugh

--- Index.php
+++ Index.php
@@ -1,7 +1,8 @@
-<?php include('single.php'); ?>
-
 <?php
 	$the_title = 'Home';
 	$the_content = '<p>This is the home page. This is where all the overview information will be displayed.</p>';
 	$ImageLocation = "/Photos/HomePhoto.jpg";
+	$ImageLocation = "/Photos/CoffeePhoto.jpg";
 ?>
+
+<?php include('single.php'); ?>
