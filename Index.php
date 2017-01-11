f8bf4c153e8746d3c79a3e7ba5ff1120e52718cc
John <howlanjo@mail.gvsu.edu>
2017-01-10 21:08:12 -0500

    eee

--- Index.php
+++ Index.php
@@ -1,10 +1,8 @@
-
-
 <?php
+	$ImageLocation = "/Photos/HomePhoto.jpg";
 	$the_title = 'Home';
 	$the_content = '<p>This is the home page. This is where all the overview information will be displayed.</p>';
 	$ImageLocation = "/Photos/HomePhoto.jpg";
 ?>
 
-<?php include('single.php'); 
-$ImageLocation = "/Photos/HomePhoto.jpg";?>
+<?php include('single.php'); ?>
