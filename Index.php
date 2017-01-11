4a79bc04492cd890ac97b76fca9232382ed36a79
John <howlanjo@mail.gvsu.edu>
2017-01-10 21:39:42 -0500

    ugh

--- Index.php
+++ Index.php
@@ -1,5 +1,3 @@
 <?php
-	$the_title = 'Home';
-	$the_content = '<p>This is the home page. This is where all the overview information will be displayed.</p>';
-	$ImageLocation = "/Photos/HomePhoto.jpg";
+	
 ?>
