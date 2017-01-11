9042591ceb46d739b5af309f02b0d2f55f4f53ae
John <howlanjo@mail.gvsu.edu>
2017-01-10 21:33:24 -0500

    try something

--- Home.php
+++ Home.php
@@ -3,3 +3,5 @@
 	$the_content = '<p>This is the home page. This is where all the overview information will be displayed.</p>';
 	$ImageLocation = "/Photos/HomePhoto.jpg";
 ?>
+
+<?php include('single.php'); ?>
