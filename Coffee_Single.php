10ede3ad610784c80924321c18e471e10c423f1a
John <howlanjo@mail.gvsu.edu>
2017-01-11 19:50:59 -0500

    Lets try this

--- Coffee_Single.php
+++ Coffee_Single.php
@@ -3,13 +3,13 @@
 					<h2><?php echo $the_title; ?></h2>
 					<?php
 						echo '<center><img src="'.$ImageLocation.'" alt="Invalid Photo" height="350" /></center>';
-						
-						<h2>"Toggle Switch"</h2>
-
-						<label class="switch">
-						  <input type="checkbox">
-						  <div class="slider"></div>
-						</label>
-						
 					?>
+					
+					<h2>"Toggle Switch"</h2>
+
+					<label class="switch">
+					  <input type="checkbox">
+					  <div class="slider"></div>
+					</label>
+					
 				</div>
