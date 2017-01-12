e5436c9071889a7205d3c6cc95856ee82587fda6
John <howlanjo@mail.gvsu.edu>
2017-01-11 19:21:53 -0500

    trial

--- Home_Single.php
+++ Home_Single.php
@@ -1,8 +1,10 @@
 			<div id="content">
 				<div id="main">
 					<h2><?php echo $the_title; ?></h2>
-					<?php
-						echo '<img src="'.$ImageLocation.'" alt="Invalid Photo"  style="margin:auto"/>';
-					?>
+					<div id="photo">
+						<?php
+							echo '<img src="'.$ImageLocation.'" alt="Invalid Photo"  style="margin:auto"/>';
+						?>
+					</div>
 				</div>
 		
