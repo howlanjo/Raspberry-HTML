9395c5f20831ad873e86de4e1b9e8365b1362617
John <howlanjo@mail.gvsu.edu>
2017-01-11 19:49:20 -0500

    test

--- Coffee_Single.php
+++ Coffee_Single.php
@@ -2,6 +2,14 @@
 				<div id="main">
 					<h2><?php echo $the_title; ?></h2>
 					<?php
-						echo '<center><img src="'.$ImageLocation.'" alt="Invalid Photo" height="200"/></center>';
+						echo '<center><img src="'.$ImageLocation.'" alt="Invalid Photo" height="350" /></center>';
+						
+						<h2>"Toggle Switch"</h2>
+
+						<label class="switch">
+						  <input type="checkbox">
+						  <div class="slider"></div>
+						</label>
+						
 					?>
 				</div>
--- Thermostat_Single.php
+++ Thermostat_Single.php
@@ -2,6 +2,6 @@
 				<div id="main">
 					<h2><?php echo $the_title; ?></h2>
 					<?php
-						echo '<center><img src="'.$ImageLocation.'" alt="Invalid Photo"/></center>';
+						echo '<center><img src="'.$ImageLocation.'" alt="Invalid Photo" height="350" /></center>';
 					?>
 				</div>
